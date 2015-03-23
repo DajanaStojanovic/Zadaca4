c3ace194ec1e90f4d2dd92bdcc454a0e7d7616d1
Dajana Stojanović <daja15@live.com>
2015-03-23 20:31:58 +0100

    Zadaca4

---
+++ script.js
@@ -0,0 +1,20 @@
+﻿function podaci(){
+	var ime = prompt(" Ime ");
+	var prezime = prompt(" Prezime ");
+	var spol = prompt(" Spol ");
+	var email = prompt(" E-mail ");
+	alert(ime + " " + prezime + " , " + email + " , " + spol + " su Vaši podaci ");
+	alert(" Potvrdite Vaše podatke. ");
+}
+
+function comm(){
+	var komentar = prompt(" Vaš komentar ");
+	alert(" Hvala Vam! ");
+}
+
+function veza(){
+	var prva = prompt(" Ime prvog lika ");
+	var druga = prompt(" Ime drugog lika ");
+	alert(prva + " i " + druga + " će " + Math.random()*100 + " % " + " biti u vezi " );
+}
+
